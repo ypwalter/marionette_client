f70130d8236fcfdee5e8585f9b075fbf9b54d4dc
Jonathan Griffin <jgriffin@mozilla.com>
2012-03-08 12:41:51 -0800

    Mark the battery test failure as expected so it won't turn orange on autolog

--- marionette/tests/emulator/test_battery.py
+++ marionette/tests/emulator/test_battery.py
@@ -1,9 +1,55 @@
+import unittest
 from marionette_test import MarionetteTestCase
 
 
 class BatteryTest(MarionetteTestCase):
 
-    def test1(self):
+    @unittest.expectedFailure
+    def test_chargingchange(self):
+        marionette = self.marionette
+        self.assertTrue(marionette.emulator.is_running)
+        marionette.set_script_timeout(10000)
+
+        moz_charging = marionette.execute_script("return navigator.mozBattery.charging;")
+        emulator_charging = marionette.emulator.battery.charging
+        self.assertEquals(moz_charging, emulator_charging)
+
+        # setup event listeners to be notified when the level or charging status 
+        # changes
+        self.assertTrue(marionette.execute_script("""
+        window.wrappedJSObject._chargingchanged = false;
+        navigator.mozBattery.addEventListener("chargingchange", function() {
+            window.wrappedJSObject._chargingchanged = true;
+        });
+        return true;
+    """))
+
+        # set the battery charging state, and verify
+        marionette.emulator.battery.charging = not emulator_charging
+        new_emulator_charging_state = marionette.emulator.battery.charging
+        self.assertEquals(new_emulator_charging_state, (not emulator_charging))
+
+        # verify that the 'chargingchange' listener was hit
+        charging_changed = marionette.execute_async_script("""
+        var callback = arguments[arguments.length - 1];
+        function check_charging_change() {
+            if (window.wrappedJSObject._chargingchanged) {
+                callback(window.wrappedJSObject._chargingchanged);
+            }
+            else {
+                setTimeout(check_charging_change, 500);
+            }
+        }
+        setTimeout(check_charging_change, 0);
+    """)
+        self.assertTrue(charging_changed)
+
+        # if we have set the charging state to 'off', set it back to 'on' to prevent
+        # the emulator from sleeping
+        if not new_emulator_charging_state:
+            marionette.emulator.battery.charging = True
+
+    def test_levelchange(self):
         marionette = self.marionette
         self.assertTrue(marionette.emulator.is_running)
         marionette.set_script_timeout(10000)
@@ -13,20 +59,12 @@ class BatteryTest(MarionetteTestCase):
         moz_level = marionette.execute_script("return navigator.mozBattery.level;")
         self.assertEquals(moz_level, marionette.emulator.battery.level)
 
-        moz_charging = marionette.execute_script("return navigator.mozBattery.charging;")
-        emulator_charging = marionette.emulator.battery.charging
-        self.assertEquals(moz_charging, emulator_charging)
-
         # setup event listeners to be notified when the level or charging status 
         # changes
         self.assertTrue(marionette.execute_script("""
         window.wrappedJSObject._levelchanged = false;
-        window.wrappedJSObject._chargingchanged = false;
         navigator.mozBattery.addEventListener("levelchange", function() {
             window.wrappedJSObject._levelchanged = true;
-        });
-        navigator.mozBattery.addEventListener("chargingchange", function() {
-            window.wrappedJSObject._chargingchanged = true;
         });
         return true;
     """))
@@ -56,32 +94,6 @@ class BatteryTest(MarionetteTestCase):
         setTimeout(check_level_change, 0);
     """)
         self.assertTrue(level_changed)
-
-        # set the battery charging state, and verify
-        marionette.emulator.battery.charging = not emulator_charging
-        new_emulator_charging_state = marionette.emulator.battery.charging
-        self.assertEquals(new_emulator_charging_state, (not emulator_charging))
-
-        # verify that the 'chargingchange' listener was hit
-        charging_changed = marionette.execute_async_script("""
-        var callback = arguments[arguments.length - 1];
-        function check_charging_change() {
-            if (window.wrappedJSObject._chargingchanged) {
-                callback(window.wrappedJSObject._chargingchanged);
-            }
-            else {
-                setTimeout(check_charging_change, 500);
-            }
-        }
-        setTimeout(check_charging_change, 0);
-    """)
-        self.assertTrue(charging_changed)
-
-        # if we have set the charging state to 'off', set it back to 'on' to prevent
-        # the emulator from sleeping
-        if not new_emulator_charging_state:
-            marionette.emulator.battery.charging = True
-
 
 
 
